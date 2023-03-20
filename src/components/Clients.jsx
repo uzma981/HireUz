2ee7a41915a737038d19b05e4ee0754934ba4222
Uzma89 <uzma.a786@hotmail.com>
2023-03-20 17:18:13 +0000

    added content to client page

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,13 +7,55 @@ export default function Clients() {
         <div className="mr-3 ml-3">
           <h2 className="mb-5 font-bold text-5xl ">CLIENTS PAGE</h2>
           <p className="justify-center items-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
-            porttitor eget dolor morbi non arcu risus quis. Risus quis varius
-            quam quisque id. In ornare quam viverra orci.
+            At Hire Uz, we are committed to promoting diversity and inclusion in
+            the workplace. As a recruitment agency, we understand the importance
+            of building a team that reflects the diverse communities we serve is
+            crucial for success. That's why we are dedicated to helping you find
+            and hire talented women who can bring a fresh perspective and unique
+            skills to your organization.Contact us today to learn more about our
+            recruitment services and how we can help you achieve your hiring
+            goals.
           </p>
         </div>
       </section>
+      <section className="bg-gray-200 py-8">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-bold mb-4 text-center">
+            RECRUITMENT SOLUTIONS
+          </h1>
+          <p className="mb-4 text-center">
+            We offer recruitment solutions for hiring more women in tech.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="bg-white rounded-lg shadow p-6 transform hover:-translate-y-1 transition-all duration-300">
+              <h2 className="text-lg font-bold mb-2">Candidate Sourcing</h2>
+              <p>
+                We use a variety of sourcing methods, such as job boards, social
+                media, and referrals, to identify and attract female candidates
+                in the technology industry.
+              </p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6 transform hover:-translate-y-1 transition-all duration-300">
+              <h2 className="text-lg font-bold mb-2">Skills assessments</h2>
+              <p>
+                We administer skills assessments and provide training to ensure
+                that candidates have the necessary technical skills for the job.
+              </p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6 transform hover:-translate-y-1 transition-all duration-300">
+              <h2 className="text-lg font-bold mb-2">
+                Talent pipeline development
+              </h2>
+              <p>
+                We work with clients to develop a long-term talent pipeline that
+                includes outreach to women's groups and organizations, as well
+                as targeted advertising and recruitment campaigns.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <div className="bg-black py-10 md:px-10 md:py-10 ">
         <section className="mt-5 mb-5 ">
           <div className="mr-3 ml-3 flex flex-col md:flex-row md:items-center">
