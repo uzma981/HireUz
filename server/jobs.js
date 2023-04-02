11b823d11255d09326b8ecf0bfccc5827fae43ce
Uzma89 <uzma.a786@hotmail.com>
2023-04-02 23:16:28 +0100

    extra fields for job schema

--- server/jobs.js
+++ server/jobs.js
@@ -9,20 +9,65 @@ const jobSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  employeeType: {
-    type: String,
-    required: true,
-  },
   location: {
     type: String,
     required: true,
   },
   salary: {
-    type: Number,
+    type: String,
     required: true,
   },
+  employeeType: {
+    type: String,
+    required: true,
+  },
+  jobDescription: [
+    {
+      jobDes1: {
+        type: String,
+      },
+      jobDes2: {
+        type: String,
+      },
+      jobDes3: {
+        type: String,
+      },
+    },
+  ],
+  jobDuties: [
+    {
+      jobDuties1: {
+        type: String,
+      },
+      jobDuties2: {
+        type: String,
+      },
+      jobDuties3: {
+        type: String,
+      },
+      jobDuties4: {
+        type: String,
+      },
+    },
+  ],
+  jobExperience: [
+    {
+      jobExp1: {
+        type: String,
+      },
+      jobExp2: {
+        type: String,
+      },
+      jobExp3: {
+        type: String,
+      },
+      jobExp4: {
+        type: String,
+      },
+    },
+  ],
 });
 
-const Job = mongoose.model('Jobs', jobSchema);
+const Job = mongoose.model('Job', jobSchema);
 
 module.exports = Job;
