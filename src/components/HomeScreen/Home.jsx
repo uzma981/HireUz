256242a5e322d421b408d25dde27930850c7a6d1
Uzma89 <uzma.a786@hotmail.com>
2023-04-20 18:12:19 +0100

    added footer to home page

--- src/components/HomeScreen/Home.jsx
+++ src/components/HomeScreen/Home.jsx
@@ -5,6 +5,7 @@ import {
   AiFillLinkedin,
   AiFillFacebook,
 } from 'react-icons/ai';
+import Footer from '../Footer';
 import { Link } from 'react-router-dom';
 export default function Home() {
   return (
@@ -26,24 +27,6 @@ export default function Home() {
           <AiFillFacebook />
         </div>
         <div className="text-left md:text-center">
-          {/* <a
-            href="#"
-            className="inline-flex items-end  py-3 text-base font-medium text-white rounded-lg hover:text-black focus:ring-purple-500"
-          >
-            Find out more
-            <svg
-              className="w-5 h-5 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a> */}
           <Link
             to="/about"
             className="inline-flex items-end  py-3 text-base font-medium text-white rounded-lg hover:text-black focus:ring-purple-500"
@@ -66,6 +49,7 @@ export default function Home() {
       </div>
 
       <Introduction />
+      <Footer/>
     </div>
   );
 }
