c9f75e82354687bad6333db5054e326116feaa57
Uzma89 <uzma.a786@hotmail.com>
2023-04-20 18:07:38 +0100

    mission section in home page

--- src/components/HomeScreen/Home.jsx
+++ src/components/HomeScreen/Home.jsx
@@ -1,43 +1,71 @@
 import React from 'react';
-
+import Introduction from './Introduction';
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillFacebook,
+} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="">
-      <div className="p-10 bg-black m-5">
-        <h1 className="text-white">We are HireUz.</h1>
+      <div className=" p-5 px-10 bg-gradient-to-r from-violet-500 to-fuchsia-500 relative w-full ">
+        <h2 className="mt-5 uppercase text-left mb-1 text-3xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl md:text-center">
+          Best-in-Class Recruitment for Women in Tech
+        </h2>
+        <div className="max-w-2xl mx-auto py-5 text-gray-300 lg:mb-8 md:text-lg lg:text-xl">
+          <p className="text-left md:text-center">
+            We strive to bridge the gender gap in tech by actively recruiting,
+            retaining and promoting women in technology fields.
+          </p>
+        </div>
+
+        <div className="text-5xl flex justify-start md:justify-center gap-10 py-3 text-gray-800">
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillFacebook />
+        </div>
+        <div className="text-left md:text-center">
+          {/* <a
+            href="#"
+            className="inline-flex items-end  py-3 text-base font-medium text-white rounded-lg hover:text-black focus:ring-purple-500"
+          >
+            Find out more
+            <svg
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a> */}
+          <Link
+            to="/about"
+            className="inline-flex items-end  py-3 text-base font-medium text-white rounded-lg hover:text-black focus:ring-purple-500"
+          >
+           Find out more
+            <svg
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+        </div>
       </div>
-      <div className="p-2 bg-gray-300 md:p-10">
-        <div className="m-5 p-2 rounded-xl bg-purple-900 md:p-4 lg:p-5">
-          <h2 className="mr-5 ml-5 text-white font-bold text-2xl md:text-4xl  ">
-            Bringing change to the technology industry
-          </h2>
-          <p className="text-gray-300 text-lg mr-5 ml-5 mt-5 md:text-xl">
-            Recruiting more women in technology  doesn’t have to be
-            complicated and our vision is to raise the standard within the
-            technology and recruitment world.
-          </p>
-        </div>
-        <div className="m-5 p-4 rounded-xl bg-purple-900 md:p-6 lg:p-5">
-          <h2 className="mr-5 ml-5 text-white font-bold text-2xl md:text-4xl  ">
-            Mentorship and Networking opportunities for women in tech
-          </h2>
-          <p className="text-gray-300 text-lg mr-5 ml-5 mt-5 md:text-xl">
-            Providing a platform for women to connect with other women in the
-            industry and receive guidance and support from experienced
-            professionals
-          </p>
-        </div>
-        <div className="m-5 p-4 rounded-xl bg-purple-900 md:p-6 lg:p-5">
-          <h2 className="mr-5 ml-5 text-white font-bold text-2xl md:text-4xl  ">
-            Prioritizing gender diversity in the tech industry
-          </h2>
-          <p className="text-gray-300 text-lg mr-5 ml-5 mt-5 md:text-xl">
-            Building strong partnerships with organisations and communities to
-            raise awareness of the importance of gender diversity in the tech
-            industry.
-          </p>
-        </div>
-      </div>{' '}
+
+      <Introduction />
     </div>
   );
 }
---
+++ src/components/HomeScreen/Introduction.jsx
@@ -0,0 +1,218 @@
+import React from 'react'
+import homeImage from '../../img/Homeimage-min.jpg'
+import codingImage from '../../img/coding-min.jpg'
+export default function Introduction() {
+  return (
+    <section className="dark:bg-gray-800 dark:text-gray-100">
+      <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl dark:text-gray-50">
+            Our Mission
+          </h2>
+          <p className="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">
+            Quando cetero his ne, eum admodum sapientem ut.
+          </p>
+        </div>
+        <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+          <div>
+            <h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">
+              Bringing change to the technology industry
+            </h3>
+            <p className="mt-3 text-lg dark:text-gray-400">
+              Recruiting more women in technology doesn’t have to be complicated
+              and our vision is to raise the standard within the technology and
+              recruitment world.
+            </p>
+            <div className="mt-12 space-y-12">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                    Diversity and Inclusion
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">
+                    Increasing diversity and inclusion includes hiring a more
+                    diverse workforce, promoting equal opportunities for all,
+                    and creating an inclusive work culture that values different
+                    perspectives and experiences.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                    Prioritzing gender diversity
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">
+                    Building strong partnerships with organisations and
+                    communities to raise awareness of the importance of gender
+                    diversity in the tech industry.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div aria-hidden="true" className="mt-10 lg:mt-0">
+            <img
+              src={codingImage}
+              alt="coding on laptop screen"
+              className="mx-auto rounded-lg shadow-lg dark:bg-gray-500"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">
+                Opportunities for women in tech
+              </h3>
+              <p className="mt-3 text-lg dark:text-gray-400">
+                Despite historically low representation in the industry, there
+                are now more opportunities than ever for women in tech.
+              </p>
+              <div className="mt-12 space-y-12">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="w-7 h-7"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                      Equal Representation
+                    </h4>
+                    <p className="mt-2 dark:text-gray-400">
+                      We will help to promote women to leadership positions,
+                      ensuring equal pay and benefits for women, and creating an
+                      inclusive work culture that values and supports women's
+                      contributions.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="w-7 h-7"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                      Skills Development
+                    </h4>
+                    <p className="mt-2 dark:text-gray-400">
+                      Providing a platform for women to connect with other women
+                      in the industry and receive guidance and support from
+                      experienced professionals. Offering training programs,
+                      mentorship opportunities, and networking events to help
+                      women gain the necessary skills and knowledge to succeed
+                      in the industry.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="w-7 h-7"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                      Support
+                    </h4>
+                    <p className="mt-2 dark:text-gray-400">
+                      We have support networks and communities for women in
+                      tech, advocating for policies and practices that promote
+                      gender equality in the workplace, and raising awareness
+                      about the importance of gender diversity in the industry.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+              <img
+                src={homeImage}
+                alt="three women with laptops"
+                className="mx-auto rounded-lg shadow-lg dark:bg-gray-500"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
